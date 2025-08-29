b1421e3a61792c990906516ab6df08508827c2b1
Dantegeo2004 <da.morenorivera@ugto.mx>
2025-08-28 20:41:54 -0600

    Add files via upload

---
+++ qgis2web_2025_08_28-20_39_54_785452/styles/RedHidro_CDMX_5_style.js
@@ -0,0 +1,97 @@
+var size = 0;
+var placement = 'point';
+function categories_RedHidro_CDMX_5(feature, value, size, resolution, labelText,
+                       labelFont, labelFill, bufferColor, bufferWidth,
+                       placement) {
+                var valueStr = (value !== null && value !== undefined) ? value.toString() : 'default';
+                switch(valueStr) {case '-1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '1':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.38}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '2':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.57}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '3':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.76}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '4':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 0.95}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '5':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.14}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '6':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.52}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;
+case '7':
+                    return [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(18,123,178,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1.9}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+                    break;}};
+
+var style_RedHidro_CDMX_5 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("order_1");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'line';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    
+    var style = categories_RedHidro_CDMX_5(feature, value, size, resolution, labelText,
+                            labelFont, labelFill, bufferColor,
+                            bufferWidth, placement);
+
+    return style;
+};
